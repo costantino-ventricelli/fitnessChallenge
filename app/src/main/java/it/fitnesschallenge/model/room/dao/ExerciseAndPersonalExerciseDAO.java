6ae0152060da1a2699ac1a0ca2b1c28fc057d96e
costantino-ventricelli <costantini.ventricelli@gmail.com>
2020-03-05 19:11:34 +0100

    Connected Foreground service and UI

--- app/src/main/java/it/fitnesschallenge/model/room/dao/ExerciseAndPersonalExerciseDAO.java
+++ app/src/main/java/it/fitnesschallenge/model/room/dao/ExerciseAndPersonalExerciseDAO.java
@@ -2,6 +2,7 @@ package it.fitnesschallenge.model.room.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Transaction;
 
@@ -11,6 +12,9 @@ import it.fitnesschallenge.model.room.entity.reference.ExerciseAndPersonalExerci
 
 @Dao
 public interface ExerciseAndPersonalExerciseDAO {
+
+    @Insert
+    long insert(ExerciseAndPersonalExercise exerciseAndPersonalExercise);
 
     @Transaction
     @Query("SELECT * FROM exercise")
