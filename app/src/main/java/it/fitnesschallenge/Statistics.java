1ccc7448356777c2ade5a4957cb7a7fcc5ce4c1a
costantino-ventricelli <costantini.ventricelli@gmail.com>
2020-03-20 18:58:22 +0100

    Add ViewStatisticsActivity with relative Fragment Statistics and Room

---
+++ app/src/main/java/it/fitnesschallenge/Statistics.java
@@ -0,0 +1,30 @@
+package it.fitnesschallenge;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Statistics#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Statistics extends Fragment {
+
+    public Statistics() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/it/fitnesschallenge/adapter/SectionsPagerAdapter.java
@@ -0,0 +1,71 @@
+/**
+ * Classe auto generata che serve a fare lo switch tra i fragment.
+ */
+package it.fitnesschallenge.adapter;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import it.fitnesschallenge.R;
+import it.fitnesschallenge.Room;
+import it.fitnesschallenge.Statistics;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.statistics_tab_title, R.string.room_tab_title};
+    private final Context mContext;
+
+    /**
+     * Il costuttore è autogenerato, imposta il fragment manager dell'activity e il contesto.
+     */
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    /**
+     * Questo metodo restituisce il fragment fa caricare nel ViewPage, che dovrebbe essere simile al
+     * FrameLayout.
+     *
+     * @param position indica il tab selezionato {0,1}
+     *                 0: Statistics tab
+     *                 1: Room tab
+     * @return restituisce il fragment selezionato.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new Statistics();
+        } else {
+            return new Room();
+        }
+    }
+
+    /**
+     * Questo metodo preleva un titlolo dall'array statico.
+     *
+     * @param position il tab di riferimento
+     * @return il titolo
+     */
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    /**
+     * Questo metodo restituisce il numero di tab presenti
+     *
+     * @return numero di tab presenti.
+     */
+    @Override
+    public int getCount() {
+        return TAB_TITLES.length;
+    }
+}
