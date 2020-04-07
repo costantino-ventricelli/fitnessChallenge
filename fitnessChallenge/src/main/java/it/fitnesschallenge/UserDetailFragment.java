3b4d1000b289534b5ce57078754353ae86e11b12
DomenicoSardone <sardonedomenico220@gmai.com>
2020-04-07 17:06:47 +0200

    Update Master detail flow

--- fitnessChallenge/src/main/java/it/fitnesschallenge/UserDetailFragment.java
+++ fitnessChallenge/src/main/java/it/fitnesschallenge/UserDetailFragment.java
@@ -1,5 +1,6 @@
 package it.fitnesschallenge;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,7 +11,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -20,10 +26,13 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import it.fitnesschallenge.adapter.LineChartMakerView;
 import it.fitnesschallenge.model.ExecutionList;
+import it.fitnesschallenge.model.room.entity.ExerciseExecution;
 import it.fitnesschallenge.model.room.entity.Workout;
 import it.fitnesschallenge.model.room.entity.reference.WorkoutWithExercise;
 
@@ -41,6 +50,9 @@ public class UserDetailFragment extends Fragment {
     private List<Entry> mEntryList = new ArrayList<>();
     private LineChart mLineChart;
     private FirebaseFirestore mDatabase;
+    List<Float> media; //ogni elemento di questa lista contiene la media di un esecuzione di workout
+    List<ExerciseExecution> exerciseExecutions; //ogni elemento contiene un ExerciseExecution
+    List<List<Float>> pesi; //array che conterra un array di pesi per ogni execution list
 
     private static final String ARG_ITEM_ID = "itemId";
 
@@ -90,6 +102,10 @@ public class UserDetailFragment extends Fragment {
                 }
             }
         });
+
+        //impostiamo il grafico
+        mLineChart = rootView.findViewById(R.id.progress_chart);
+        setBarChart();
         return rootView;
     }
 
@@ -119,7 +135,17 @@ public class UserDetailFragment extends Fragment {
      * @param executionList
      */
     private void getExecutionData(ExecutionList executionList) {
-
+        pesi = new ArrayList<>();
+        exerciseExecutions = executionList.getExerciseList(); //questo metodo ci ritorna una lista di tipo exercise exection
+        for(ExerciseExecution execution : exerciseExecutions) {
+            pesi.add(execution.getUsedKilograms());
+        }
+        //quindi adesso ogni elemento della lista pesi, conntiene la lista dei pesi usati
+        media = new ArrayList<>(); //ogni elemento contiene la media
+        for(int i = 0; i < pesi.size(); i++) {
+           media.add(getKilogramsAVG(pesi.get(i)));
+        }
+        setEntryList(exerciseExecutions);
     }
 
     /**
@@ -134,4 +160,71 @@ public class UserDetailFragment extends Fragment {
         return sum / usedKilograms.size();
     }
 
+    /**
+     * Questo metodo crea la lista dei nodi necessari per disegnare il grafico in questione.
+     * @param exerciseExecutions contiene una lista delle esecuzioni.
+     */
+    private void setEntryList(List<ExerciseExecution> exerciseExecutions) {
+        for (ExerciseExecution execution : exerciseExecutions) {
+            Calendar calendar = Calendar.getInstance(Locale.getDefault());
+            calendar.setTime(execution.getExecutionDate());
+            String floatDate = calendar.get(Calendar.DAY_OF_YEAR) + "." + calendar.get(Calendar.YEAR);
+            float executionDate = Float.parseFloat(floatDate);
+            Log.d(TAG, "Data di esecuzione: " + executionDate);
+            float usedKilogramsAvg = getKilogramsAVG(execution.getUsedKilograms());
+            Entry entry = new Entry(executionDate, usedKilogramsAvg);
+            Log.d(TAG, "\tCreata nuova entry: " + executionDate + ", " + usedKilogramsAvg);
+            mEntryList.add(entry);
+        }
+    }
+
+    /**
+     * Questo metodo contiene i comandi per settare i nodi che il grafico dovrà rappresentare.
+     * Tramite la classe LineDataSet viene utilizzata la lista dei nodi da passare al grafico, impostando
+     * le dipendenze, e gli apetti delle linee di collegamento e i punti da mostrare.
+     * LineData racchiude queste informazioni e le invia al grafico per permetterne la reppresentazione.
+     */
+    private void setBarChart() {
+        LineDataSet lineDataSet = new LineDataSet(mEntryList, "Execution");
+        lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        lineDataSet.setCircleColor(getContext().getColor(R.color.colorPrimaryDark));
+        lineDataSet.setLineWidth(4f);
+        lineDataSet.setCircleRadius(6f);
+
+        LineData data = new LineData(lineDataSet);
+        data.setValueTextSize(10f);
+
+        mLineChart.setData(data);
+
+        initLineChartChart();
+    }
+
+    /**
+     * Questo metodo permette di impostare i valori necessari per disegnare il grafico, come le etichette,
+     * la legenda, la granularità del grafico, e lo sfondo di tutta la rappresentazione.
+     */
+    private void initLineChartChart() {
+        Legend legend = mLineChart.getLegend();
+        legend.setEnabled(false);
+        mLineChart.setBackgroundColor(Color.WHITE);
+        mLineChart.getDescription().setEnabled(false);
+        mLineChart.setTouchEnabled(true);
+        mLineChart.setDragEnabled(true);
+        mLineChart.setPinchZoom(true);
+        mLineChart.animateX(0);
+
+        XAxis xAxis = mLineChart.getXAxis();
+        xAxis.setGranularity(1F);
+        xAxis.setXOffset(10F);
+
+
+        YAxis rightAxis = mLineChart.getAxisRight();
+        rightAxis.setDrawAxisLine(false);
+        rightAxis.setDrawTopYLabelEntry(false);
+        rightAxis.setDrawLabels(false);
+
+        LineChartMakerView makerView = new LineChartMakerView(getContext(), R.layout.custom_point_view);
+        makerView.setChartView(mLineChart);
+        mLineChart.setMarker(makerView);
+    }
 }
